eedc9cde5fcd3c9b51f87720f7bf2f8cbbe3ba46
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-01-04 12:16:36 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1684 ms, Memory: 62132 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		
+		int i=str.length()-1;
+		int sum = 0, cycle = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		while( true ) {
+			
+			if( i < 0 ) break;
+			
+			sum += (str.charAt(i)-'0') * Math.pow(2, cycle);
+			cycle++;
+			
+			if( cycle == 3 || i == 0 ) {
+				stack.push(sum);
+				cycle = 0;
+				sum = 0;
+			}
+			
+			i--;
+		}
+		
+		while( !stack.empty() ) System.out.print(stack.pop());
+	}
+
+}
