fbe63f863513d3d22f1112d25fcdf4bb585b1daa
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-01-08 10:18:17 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 112 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=N*2-1; i++) {
+			for(int j=1; j<=N; j++) {
+				if( j <= Math.abs(N-i) ) sb.append(" ");
+				else sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
