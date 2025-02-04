66ae07ad23b138642d003a543111de8d8487dddd
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-02-04 10:58:32 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 18144 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			if( i < 3 ) arr[i] = i;
+			else arr[i] = (arr[i-1] + arr[i-2]) % 15746;
+		}
+		System.out.println(arr[N]);
+	}
+
+}
