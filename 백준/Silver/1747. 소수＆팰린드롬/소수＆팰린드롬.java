992fa912ce55deffd3256c2ca0f1014241948a86
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-02-20 18:05:20 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 140 ms, Memory: 15592 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*	소수&팰린드롬 */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -17,22 +18,35 @@ public class Main {
 			}
 		}
 		int num = N;
-		boolean isPalin = true;
+		boolean isPalin = false;
 		while( true ) {
 			// 팰린드롬인지 판단
-			String pNum = Integer.toString(num);
-			for(int i=0; i<pNum.length()/2; i++) {
-				if( pNum.charAt(i) != pNum.charAt(pNum.length()-1-i) ) {
-					isPalin = false;
+			// 1) 문자열로 확인
+//			String pNum = Integer.toString(num);
+//			for(int i=0; i<pNum.length()/2; i++) {
+//				if( pNum.charAt(i) != pNum.charAt(pNum.length()-1-i) ) {
+//					isPalin = false;
+//					break;
+//				}
+//			}
+			
+			// 2) 사칙연산 이용하여 뒤부터 읽은 숫자 만들기
+			int remain = num % 10;
+			int tmp = num;
+			while( true ) {
+				if( tmp / 10 == 0 ) {
 					break;
 				}
+				tmp /= 10;
+				remain = remain * 10 + tmp % 10;
 			}
+			if( num == remain ) isPalin = true;
 			
 			// 팰린드롬이면 루프 탈출
 			if( !arr[num] && isPalin ) break;
 			
 			num++;
-			isPalin = true;
+			isPalin = false;
 		}
 		System.out.println(num);
 	}
