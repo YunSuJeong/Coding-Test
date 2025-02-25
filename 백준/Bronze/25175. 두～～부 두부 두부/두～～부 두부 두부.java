f14307ac79e3be10d7b79df554688dab8de1b314
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-02-25 18:34:06 +0900

    [Bronze III] Title: 두~~부 두부 두부, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.java
+++ 백준/Bronze/25175. 두～～부 두부 두부/두～～부 두부 두부.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
+/*	두~~부 두부 두부 */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -16,16 +17,21 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int mo = 3;
-		for(int i=M; i<M+N; i++) {
-			map.put(mo%N, i%N == 0 ? N : i%N);
-			mo++;
-		}
+		// 1. 번호, 두부 번호 매핑
+//		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+//		int mo = 3;
+//		for(int i=M; i<M+N; i++) {
+//			map.put(mo%N, i%N == 0 ? N : i%N);
+//			mo++;
+//		}
+//		
+//		int key = K % N;
+//		if( key < 0 ) key += N;
+//		System.out.println(map.get(key));
 		
-		int key = K % N;
-		if( key < 0 ) key += N;
-		System.out.println(map.get(key));
+		// 2. 식으로 한번에 찾기
+		int answer = (M + (K-3) % N + N) % N;
+		System.out.println(answer == 0 ? N : answer);
 	}
 
 }
