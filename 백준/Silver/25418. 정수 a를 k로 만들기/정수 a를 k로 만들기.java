6c39fc0968cb6b45a176fedb69c9d7e36fb2300c
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-03-08 15:29:43 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,29 +13,43 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[] dp = new int[1000001];			// 최소 연산 횟수
-
-		if( K - N > 1 ) {
-			// 1씩 늘어나는 연산
-			for(int i=N; i<=K; i++) {
-				if( i == N ) dp[i] = 0;
-				else {
-					if( dp[i] == 0 ) dp[i] = dp[i-1] + 1;
-					else dp[i] = Math.min(dp[i], dp[i-1]+1);
-				}
-				
-				int idx = i;
-				// 2번 연산만 한 경우
-				while( true ) {
-					if( idx * 2 > K ) break;
-					if( dp[idx*2] == 0 ) dp[idx*2] = dp[idx] + 1; 
-					else dp[idx*2] = Math.min(dp[idx*2], dp[idx] + 1);
-					idx *= 2;
-				}
-			}
-			System.out.println(dp[K]);
-		}
+		// 1. dp
+//		int[] dp = new int[1000001];			// 최소 연산 횟수
+//
+//		if( K - N > 1 ) {
+//			for(int i=N; i<=K; i++) {
+//				if( i == N ) dp[i] = 0;			// 기저값 : N일때 최소연산 횟수 0
+//				else {
+//					// 1번 연산만 하여 최솟값 세팅
+//					if( dp[i] == 0 ) dp[i] = dp[i-1] + 1;
+//					else dp[i] = Math.min(dp[i], dp[i-1]+1);
+//				}
+//				
+//				// 현재 수에서 K보다 작을 때까지 2번 연산만 진행하여 최소 연산 횟수 세팅
+//				int idx = i;
+//				while( true ) {
+//					if( idx * 2 > K ) break;
+//					if( dp[idx*2] == 0 ) dp[idx*2] = dp[idx] + 1; 
+//					else dp[idx*2] = Math.min(dp[idx*2], dp[idx] + 1);
+//					idx *= 2;
+//				}
+//			}
+//			System.out.println(dp[K]);
+//		}
 		
+		
+		// 2. 그리디 : 2번 연산이 많을수록 최소횟수가 나오므로 2로 나눠지면 2번연산 수행, 나눠지지 않으면 1번연산 수행하며 연산횟수 찾기
+		int cnt = 0;
+		while( true ) {
+			if( K == N ) break;
+			if( K % 2 == 0 && K / 2 >= N) {
+				K /= 2;
+			} else {
+				K--;
+			}
+			cnt++;
+		}
+		System.out.println(cnt);
 	}
 
 }
