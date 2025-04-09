633d2fb209ed3f44c385b23195293f26ad7d8a93
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-04-09 18:01:23 +0900

    [D3] Title: 격자판 칠하기, Time: 117 ms, Memory: 31,976 KB -BaekjoonHub

--- SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.java
+++ SWEA/D3/14413. 격자판 칠하기/격자판 칠하기.java
@@ -1,9 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
+	
+	public static int N, M;
+	public static boolean flag = true;
+	public static int[][] arr;
+	public static boolean[][] visited;
+	
+	public static int[] X = {-1, 1, 0, 0};
+	public static int[] Y = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,10 +24,11 @@ public class Solution {
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
 			
-			int[][] arr = new int[N][M];
+			arr = new int[N][M];
+			visited = new boolean[N][M];
 			
 			for(int i=0; i<N; i++) {
 				String str = br.readLine();
@@ -30,7 +41,8 @@ public class Solution {
 				}
 			}
 			
-			boolean flag = true;
+			// 1) 확인해야하는 두가지 경우 나누어 해결
+			/*flag = true;
 			// 흑으로 시작하는 격자판
 			for(int i=0; i<N; i++) {
 				int n = i % 2 == 0 ? 1 : 2;
@@ -48,7 +60,7 @@ public class Solution {
 
 			// 백으로 시작하는 격자판
 			if( !flag ) {
-                flag = true;
+				flag = true;
 				for(int i=0; i<N; i++) {
 					int n = i % 2 == 0 ? 2 : 1;
 					for(int j=0; j<M; j++) {
@@ -62,9 +74,49 @@ public class Solution {
 					}
 					if( !flag ) break;
 				}
+			}*/
+			
+			// 2) bfs로 해결해보기
+			flag = true;
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<M; j++) {
+					if( arr[i][j] > 0 && flag ) bfs(i, j);
+				}
 			}
 			
 			System.out.printf("#%d %s\n", tc, flag ? "possible" : "impossible");
+		}
+	}
+	
+	public static void bfs(int x, int y) {
+		Queue<int[]> queue = new LinkedList<int[]>();
+
+		queue.offer(new int[] {x, y});
+		visited[x][y] = true;
+		
+		while( !queue.isEmpty() ) {
+			if( !flag ) break;
+			
+			int[] tmp = queue.poll();
+			
+			x = tmp[0];
+			y = tmp[1];
+			
+			for(int i=0; i<4; i++) {
+				int a = x + X[i];
+				int b = y + Y[i];
+				
+				if( a >= 0 && a < N && b >= 0 && b < M && !visited[a][b] ) {
+					if( arr[a][b] > 0 && arr[a][b] == arr[x][y] ) {
+						flag = false;
+						break;
+					} else {
+						visited[a][b] = true;
+						queue.offer(new int[] {a, b});
+						arr[a][b] = arr[x][y] == 1 ? 2 : 1;
+					}
+				}
+			}
 		}
 	}
 
