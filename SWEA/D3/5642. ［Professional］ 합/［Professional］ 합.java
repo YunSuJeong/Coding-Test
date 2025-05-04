96f65e33e0091f3f826dd326af753339c8945ff5
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-05-04 23:21:04 +0900

    [D3] Title: [Professional] 합, Time: 283 ms, Memory: 98,040 KB -BaekjoonHub

---
+++ SWEA/D3/5642. ［Professional］ 합/［Professional］ 합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc=1; tc<=T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int[] arr = new int[N];
+			int[] dp = new int[N];
+			for(int i=0; i<N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+				dp[i] = arr[i];
+			}
+			
+			int max = dp[0];
+			for(int i=1; i<N; i++) {
+				dp[i] = Math.max(dp[i], dp[i-1] + arr[i]);
+				max = Math.max(max, dp[i]);
+			}
+			
+			System.out.printf("#%d %d\n", tc, max);
+		}
+	}
+
+}
