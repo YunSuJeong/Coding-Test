1790c2db7045fae4e2ee2dd60ebc75da5fccd4d4
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-05-07 17:04:42 +0900

    [Silver I] Title: 계단, Time: 292 ms, Memory: 25984 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/21600. 계단/계단.java
+++ 백준/Silver/21600. 계단/계단.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +13,9 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int[] arr = new int[N];
-		int[][] dp = new int[N+1][N];
+		
+		// 1. 2차원으로 설계하면 메모리 및 시간초과 O(n^2)
+		/*int[][] dp = new int[N+1][N];
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			dp[1][i] = 1;
@@ -26,10 +29,25 @@ public class Main {
 					dp[i][j] = dp[i-1][j];
 				}
 			}
+		}*/
+
+		// 2. 1차원으로 설계하여 풀어야함
+		int[] dp = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		dp[0] = 1;
+        // 현재 히스토그램 높이 >= 이번 계단 높이 : 바로 이전 히스토그램까지의 계단 최대 길이 + 1
+        // 현재 히스토그램 높이 < 이번 계단 높이 : 이번 히스토그램 높이
+		for(int i=1; i<N; i++) {
+			dp[i] = Math.min(dp[i-1] + 1, arr[i]);
+		}
+
+		
+		
 		int max = 0;
-		for(int i=0; i<N; i++) max = Math.max(max, dp[N][i]);
+		for(int i=0; i<N; i++) max = Math.max(max, dp[i]);
 		System.out.println(max);
 	}
 
