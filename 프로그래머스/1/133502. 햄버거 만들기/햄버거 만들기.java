92fddfb5c4c4be6db695191500b3020aa5031dd8
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-09-10 14:58:46 +0900

    [level 1] Title: 햄버거 만들기, Time: 18.96 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,40 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
         int answer = 0;
-        
-        int[] burger = {1, 2, 3, 1};
-        int[] check = new int[4];
-        Stack<Integer> stack = new Stack<>();
-        
-        boolean isBurger = true;
-        for(int i=0; i<=ingredient.length; i++) {
-            if( stack.size() >= 4 ) {
-                isBurger = true;
-                for(int j=3; j>=0; j--) {
-                    check[j] = stack.pop();
-                }
-                
-                for(int j=0; j<4; j++) {
-                    if( check[j] != burger[j] ) {
-                        isBurger = false;
-                        break;
-                    }
-                }
-                
-                if( isBurger ) {
-                    answer++;
-                } else {
-                    for(int j=0; j<4; j++) {
-                        stack.push(check[j]);
-                    }
-                }
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
             }
-            
-            if( i < ingredient.length ) stack.push(ingredient[i]);
         }
-        
         return answer;
     }
 }
