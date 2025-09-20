76760c0d22870cd57c0de160ee1bc0af985321fd
YunSuJeong <91771574+YunSuJeong@users.noreply.github.com>
2025-09-20 14:22:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.30 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        int YYYY = Integer.parseInt(today.split("\\.")[0]);
+        int MM = Integer.parseInt(today.split("\\.")[1]);
+        int DD = Integer.parseInt(today.split("\\.")[2]);
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(String t : terms) {
+            map.put(t.split(" ")[0], Integer.parseInt(t.split(" ")[1]));
+        }
+        
+        for(int i=0; i<privacies.length; i++) {
+            String info = privacies[i].split(" ")[0];
+            String type = privacies[i].split(" ")[1];
+            int period = map.get(type);
+            
+            int year = Integer.parseInt(info.split("\\.")[0]) + period / 12;
+            int month = Integer.parseInt(info.split("\\.")[1]) + period % 12;
+            int day = Integer.parseInt(info.split("\\.")[2]);
+            
+            if( month > 12 ) {
+                year += month / 12;
+                month = month % 12 == 0 ? month : month % 12;
+            }
+            
+            if( ( (year < YYYY) || (year == YYYY && month < MM )) || (year == YYYY && month == MM && day <= DD) ) answer.add(i+1);
+        }
+        
+        return answer;
+    }
+}
